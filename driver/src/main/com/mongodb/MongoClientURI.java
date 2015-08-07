e7acdd5f8076f83eed379db4c21dca7b699c2f0a
Ross Lawley <ross.lawley@gmail.com>
2015-08-07 17:06:16 +0100

    Bring ConnectionString up to Spec.
    Update the class to conform to the Spec
    Implement Spec test cases
    JAVA-1891

--- driver/src/main/com/mongodb/MongoClientURI.java
+++ driver/src/main/com/mongodb/MongoClientURI.java
@@ -225,7 +225,7 @@ public class MongoClientURI {
      * @return the URI
      */
     public String getURI() {
-        return proxied.getURI();
+        return proxied.getConnectionString();
     }
 
     /**
