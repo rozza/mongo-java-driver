a9ab3da3d092eaee779211334bc2905a42e0576f
Ross Lawley <ross.lawley@gmail.com>
2015-09-01 10:10:39 +0100

    Fix checkClosed data race

--- driver/src/main/com/mongodb/client/gridfs/GridFSUploadStreamImpl.java
+++ driver/src/main/com/mongodb/client/gridfs/GridFSUploadStreamImpl.java
@@ -70,8 +70,8 @@ final class GridFSUploadStreamImpl extends GridFSUploadStream {
 
     @Override
     public void abort() {
-        checkClosed();
         synchronized (closeLock) {
+            checkClosed();
             closed = true;
         }
         chunksCollection.deleteMany(new Document("files_id", fileId));
@@ -165,8 +165,10 @@ final class GridFSUploadStreamImpl extends GridFSUploadStream {
     }
 
     private void checkClosed() {
-        if (closed) {
-            throw new MongoGridFSException("The OutputStream has been closed");
+        synchronized (closeLock) {
+            if (closed) {
+                throw new MongoGridFSException("The OutputStream has been closed");
+            }
         }
     }
 
