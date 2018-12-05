7747ae66344e8425564d232c9e91eed2550e8467
Ross Lawley <ross.lawley@gmail.com>
2018-12-05 15:36:53 +0000

    Quite the logs and fail when correctly

--- .evergreen/run-embedded-tests-android.sh
+++ .evergreen/run-embedded-tests-android.sh
@@ -10,7 +10,9 @@
 SDK_HOME=$PWD/.android
 if [ ! -e  $SDK_HOME ]; then
     echo "Installing ANDROID SDK"
+    DOWNLOAD_LOGS=${SDK_HOME}/download_logs
     mkdir -p $SDK_HOME
+    mkdir -p $DOWNLOAD_LOGS
     (
         cd $SDK_HOME
         export JAVA_HOME="/opt/java/jdk8"
@@ -20,8 +22,8 @@ if [ ! -e  $SDK_HOME ]; then
         export ANDROID_SDK_HOME=${SDK_HOME}
 
         SDK_PACKAGE=sdk-tools-linux-4333796.zip
-        curl -O https://dl.google.com/android/repository/$SDK_PACKAGE
-        unzip $SDK_PACKAGE
+        curl -O -s https://dl.google.com/android/repository/$SDK_PACKAGE
+        unzip -o -qq $SDK_PACKAGE
         yes | $SDK_HOME/tools/bin/sdkmanager --channel=0 \
             "platforms;android-28"  \
             "emulator" \
@@ -32,9 +34,9 @@ if [ ! -e  $SDK_HOME ]; then
 
         $SDK_HOME/tools/bin/sdkmanager --update
         PLATFORM_TOOLS=platform-tools-latest-linux.zip
-        curl -O https://dl.google.com/android/repository/$PLATFORM_TOOLS
-        unzip -o $PLATFORM_TOOLS
-    )
+        curl -O -s https://dl.google.com/android/repository/$PLATFORM_TOOLS
+        unzip -o -qq $PLATFORM_TOOLS
+    ) &> $DOWNLOAD_LOGS/download.log
 fi
 
 (
@@ -58,10 +60,11 @@ fi
       if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
         || "$bootanim" =~ "running" ]]; then
         let "failcounter += 1"
-        echo "Waiting for emulator to start"
-        if [[ $failcounter -gt timeout_in_sec ]]; then
+        if [[ "$failcounter" -gt timeout_in_sec ]]; then
           echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
           exit 1
+        elif (( "$failcounter" % 10 )); then
+           echo "Waiting for emulator to start"
         fi
       fi
       sleep 5
@@ -73,7 +76,9 @@ export JAVA_HOME="/opt/java/jdk9"
 export ANDROID_HOME=${SDK_HOME}
 
 function shutdownEmulators {
+  retVal=$?
   $SDK_HOME/platform-tools/adb devices | grep emulator | cut -f1 | while read line; do $SDK_HOME/platform-tools/adb -s $line emu kill; done;
+  exit $retVal
 }
 
 trap shutdownEmulators EXIT
